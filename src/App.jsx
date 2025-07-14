774577c9927d94b84e0efddb0afd608336352648
Ryan Hockett <ryanhockett.dev@gmail.com>
2025-07-13 23:27:59 -0500

    Added Dashboard functionality/template, added sample data to be passed to Dashboard view through state variabe, added JobCard component functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,85 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
 
-import Dashboard from './pages/Dashboard.jsx'
+import Dashboard from './pages/Dashboard.jsx';
 
 function App() {
-  
+  // Initialize the state to empty arrays
+  const [jobs, setJobs] = useState ([]);
+  const [prepItems, setPrepItems] = useState([]);
 
-  return <Dashboard/>
+  // temp variables for holding sample data
+  const job1 = {
+    id: 1,
+      company: 'Google',
+      role: 'Frontend Developer',
+      source: 'LinkedIn',
+      dateApplied: '2025-07-12',
+      status: 'Rejected',
+      notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+  };
+  const job2 = {
+    id: 2,
+      company: 'Apple',
+      role: 'Frontend Developer',
+      source: 'Company Site',
+      dateApplied: '2025-07-13',
+      status: 'Applied',
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+  };
+  const prep1 = {
+    id: 1,
+    type: "concept",
+    title: "React.js study notes",
+    notes: "React is a JS library that focuses on building single-page applications through component based architecture and updating a virtual copy of the DOM",
+    tags: ["Javascript", "React", "Frontend"],
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  }
+  const prep2 = {
+    id: 2,
+    type: "question",
+    title: "What is useState() in React?",
+    notes: "The useState() is a built in React hook that allows state variables to be used across multiple decoupled components, being updated and manipulated by these components",
+    tags: ["Javascript", "React", "Frontend"],
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  }
+
+  //Functions for abstracting the addition of a job or prep item to the state array, ensuring the previous state values are always used
+  const handleAddJob = (newJob) => {
+  setJobs((prev) =>
+    [...prev, newJob].sort(
+      //Sorts the new item by if it is more recent (by updatedAt date) than each item in the prev array
+      (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)
+      )
+    );
+  };
+
+  const handleAddPrep = (newPrep) => {
+    setPrepItems((prev) =>
+    [...prev, newPrep].sort(
+      (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)
+      )
+    );
+  };
+
+  //functions to keep useEffect clean
+  const loadMockData = () => {
+    handleAddJob(job1);
+    handleAddJob(job2);
+    handleAddPrep(prep1);
+    handleAddPrep(prep2);
+  }
+  //useEffect with an empty dependency array ensures that these functions are only called one after the component mounts, and the temporary manual data is only loaded in once
+  useEffect(() => {
+  loadMockData();
+}, []);
+
+  //Returns the Dashboard view passing the jobs and prepItems state variable arrays
+  return <Dashboard jobs={jobs} prepItems={prepItems}/>
 }
 
 
---
+++ src/components/JobCard.jsx
@@ -0,0 +1,64 @@
+import { Container, Card, Badge, ListGroup } from 'react-bootstrap';
+
+
+function JobCard({job}) {
+    let status = ""
+    switch(job.status) {
+        case 'Applied':
+            status = "primary";
+            break;
+        case 'Interviewing':
+            status = "warning";
+            break;
+        case 'Offer':
+            status = "success";
+            break;
+        case 'Rejected':
+            status = "danger";
+            break;
+        case 'Withdrawn':
+            status = "info";
+            break;
+        default:
+            status = "primary"
+    }
+    return (
+        <Card className='h-100'>
+            <Card.Header className='d-flex justify-content-between align-items-center'>Job Application<h5><Badge bg={status}>{job.status}</Badge></h5></Card.Header>
+            
+            <Card.Body>
+                
+                <Card.Title className='border-bottom py-2'>Company: {job.company}</Card.Title>
+                
+                
+                <ListGroup>
+                <ListGroup.Item>Role: {job.role}</ListGroup.Item>
+                <ListGroup.Item>Source: {job.source}</ListGroup.Item>
+                <ListGroup.Item>Date Applied: {job.dateApplied}</ListGroup.Item>
+                {job.notes && (
+                    <ListGroup.Item className='notes'>Notes: {job.notes}</ListGroup.Item>
+                )}
+                {job.location && (
+                    <ListGroup.Item>Location: {job.location}</ListGroup.Item>
+                )}
+                {job.salaryFloor && job.salaryCeiling && (
+                    <ListGroup.Item>Salary Range: ${job.salaryFloor} - {job.salaryCeiling}</ListGroup.Item>
+                )}
+                
+                {job.contactName && (
+                    <ListGroup.Item>Contact: {job.contactName}</ListGroup.Item>
+                )}
+                {job.contactInfo && (
+                    <ListGroup.Item>Contact info: {job.contactInfo}</ListGroup.Item>
+                )}
+                </ListGroup>
+            </Card.Body>
+            <Card.Footer>
+                <Card.Text>Created: {job.createdAt}</Card.Text>
+                <Card.Text>Updated: {job.updatedAt}</Card.Text>
+            </Card.Footer>
+        </Card>
+    )
+}
+
+export default JobCard;
